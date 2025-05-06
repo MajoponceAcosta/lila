b94436cc381cf8252bbf538d34457616508ff86f
MajoponceAcosta <agoria10733@gmail.com>
2025-05-06 18:55:08 -0500

    Add files via upload

---
+++ ponac/lila.js
@@ -0,0 +1,15 @@
+// Solicitar la edad al usuario
+const edad = prompt("Por favor, ingresa tu edad:");
+
+// Verificar si el usuario es mayor o menor de edad
+if (edad >= 18) {
+    alert("Eres mayor de edad.");
+} else {
+    alert("Eres menor de edad.");
+}
+
+// Imprimir los números del 1 al 10 en la consola
+console.log("Números del 1 al 10:");
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
